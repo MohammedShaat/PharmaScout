dd64b8e91268483954cd45fad0f91001547655ec
Mohammed Shaat <mohammedshaat.it@gmail.com>
2026-08-26 09:04:10 -0400

    Initial Commit

---
+++ PharmaScout/PharmaScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  PharmaScoutApp.swift
+//  PharmaScout
+//
+//  Created by Mohammed on 8/26/26.
+//
+
+import SwiftUI
+
+@main
+struct PharmaScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
